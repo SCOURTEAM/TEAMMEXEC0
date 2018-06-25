f76fc1ca0c3c6da0856eccc8b5872123c45dff17
SCOURTEAM <40240521+SCOURTEAM@users.noreply.github.com>
2018-06-26 00:13:43 +0300

    Update super_group.lua

--- plugins/super_group.lua
+++ plugins/super_group.lua
@@ -1,6 +1,6 @@
 --[[
 << تم برمجه وكتابه السورس من قبل مطورين >>
-<<         @BLCON  \  @TH3BOSS          >>
+<<         @MEXECO  \  @VI_P2          >>
 ]]
 local function xboss(msg, matches)
 if msg.to.type ~= 'pv' then
