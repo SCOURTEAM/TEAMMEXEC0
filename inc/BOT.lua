0ecd50d301bcb4c77c9ab83c0ffca25e5120101b
SCOURTEAM <40240521+SCOURTEAM@users.noreply.github.com>
2018-06-25 23:21:26 +0300

    Update BOT.lua

--- inc/BOT.lua
+++ inc/BOT.lua
@@ -1,6 +1,6 @@
 --[[
 << تم برمجه وكتابه السورس من قبل مطورين >>
-<<         @BLCON  \  @TH3BOSS          >>
+<<         @MEXECO  \  @VI_P2          >>
 ]] 
 function load_proc()
 loadfile('./inc/functions.lua')()
